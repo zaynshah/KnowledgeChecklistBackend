933b6884669919a3641ba3c3a19d15ae911089dd
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2022-02-22 09:33:47 +0000

    Created seed data for backend

---
+++ seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'HTML/CSS', 'Understand what parent and child is');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'HTML/CSS', 'Can create and link a stylesheet');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'Javascript', 'Be able to link a Javascript file in your project');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'Javascript', 'Be able to do a console.log()');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Understand the difference between class and functional components');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Be able to create a React application with create-react-app');
+
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'HTML/CSS', 'Understand what parent and child is');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'HTML/CSS', 'Can create and link a stylesheet');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'Javascript', 'Be able to link a Javascript file in your project');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'Javascript', 'Be able to do a console.log()');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'React', 'Understand the difference between class and functional components');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'React', 'Be able to create a React application with create-react-app');
+
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (3, 'HTML/CSS', 'Understand what parent and child is');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (3, 'HTML/CSS', 'Can create and link a stylesheet');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (3, 'Javascript', 'Be able to link a Javascript file in your project');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (3, 'Javascript', 'Be able to do a console.log()');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (3, 'React', 'Understand the difference between class and functional components');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (3, 'React', 'Be able to create a React application with create-react-app');
+
