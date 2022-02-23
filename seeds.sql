fac2d74d446055db84657be40e75c8b7b77aa38b
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2022-02-23 17:48:47 +0000

    Updated tables

--- seeds.sql
+++ seeds.sql
@@ -2,13 +2,21 @@ INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1,
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'HTML/CSS', 'Can create and link a stylesheet');
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'Javascript', 'Be able to link a Javascript file in your project');
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'Javascript', 'Be able to do a console.log()');
-INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Understand the difference between class and functional components');
-INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Be able to create a React application with create-react-app');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Be able to pass props');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Be able to handle events in React');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Be able to use ternary operators in React');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Be able to use conditional rendering');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Understand what state is and how to use it');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Be able to update state');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (1, 'React', 'Understand what hooks are');
 
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'HTML/CSS', 'Understand what parent and child is');
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'HTML/CSS', 'Can create and link a stylesheet');
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'Javascript', 'Be able to link a Javascript file in your project');
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'Javascript', 'Be able to do a console.log()');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'Javascript', 'Be able to use built-in object methods like .keys()');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'Javascript', 'Be able to loop over an object with for...in');
+INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'Javascript', 'Be able to convert an object into an array');
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'React', 'Understand the difference between class and functional components');
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (2, 'React', 'Be able to create a React application with create-react-app');
 
@@ -19,3 +27,4 @@ INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (3,
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (3, 'React', 'Understand the difference between class and functional components');
 INSERT INTO learning_objectives(cohort_id, topic, learning_objective) VALUES (3, 'React', 'Be able to create a React application with create-react-app');
 
+INSERT INTO users(email, cohort_id, encrypted_password, created_at, updated_at, admin) VALUES('admin@sigmalabs.co.uk', 0, '$2a$08$1pO8zSzvfNwvyW/IRRdgk.Ac7cYQHKEob4kjGEZZqcVUfS/VyikKm', datetime('now'), datetime('now'), 1)
